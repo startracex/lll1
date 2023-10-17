dcda0aafcb6fe4b6cfbf5f19475bc06a4b51401f
STARTRACEX <startraceebr@foxmail.com>
2023-10-17 16:01:29 +0800

    Update with.ts

--- src/with.ts
+++ src/with.ts
@@ -1,72 +1,76 @@
 import { conf } from "./conf.js";
 export const define = (name: string, options?: ElementDefinitionOptions) => (constructor: CustomElementConstructor) => {
-  const tagname = conf?.tag(name);
+  const tagname = conf.tag(name);
   if (customElements.get(tagname) === undefined) {
     customElements.define(tagname, constructor, options);
-    conf?.enabled?.push(name);
-    conf?.namemap?.set(name, tagname);
+    conf.enabled.push(name);
+    conf.namemap.set(name, tagname);
   }
 };
+
 /**
- * Create element with args append to target
- * @param target Appended target element or use document.querySelector(target) or document.body
- * @param args tag:tag name, props:attribute, children:appended chindren, html:innerHTML
+ * Create element from args append to target
+ * @param target Appended target element, target or document.querySelector(target) or document.body
+ * @param args tag: tag name, props: attribute, children: appended chindren
  */
-export const append = (target: string | object = "body", args: string | { tag: any; props?: any; children?: any; html?: any } = "div") => {
-  if (!args) return;
-  target = (typeof target === "string" ? document.querySelector(target) : target) || document.body;
-  const element = create(args);
-  (target as HTMLElement).appendChild(element);
-};
+export function append(target: string | HTMLElement = document.body, args: string | EleArgs | HTMLElement) {
+  if (!(target instanceof HTMLElement)) {
+    target = (document.querySelector(target) || document.body) as HTMLElement;
+  }
+  target.appendChild(create(args));
+}
+
 /**
  * Create element from args
- * @param args tag:tag name, props:attribute, children:appended chindren, html:innerHTML
+ * @param args tag: tag name, props: attribute, children: appended chindren
  */
-export const create = (args: string | { tag: any; props?: any; children?: any; html?: any } = "div") => {
-  const tag = (typeof args === "string" ? args : args?.tag) || "div";
-  const { props, children, html } = (args as any) || {};
-  const element = document.createElement(tag);
+export function create(args: string | EleArgs | HTMLElement): HTMLElement {
+  if (args instanceof HTMLElement) {
+    return args;
+  }
+  const { tag, props, children } = (typeof args === "string" ? { tag: args } : args) as EleArgs;
+  const newElement = document.createElement(tag);
   if (props) {
     Object.keys(props).forEach((prop) => {
-      // element[prop] = props[prop];
-      element.setAttribute(prop, props[prop] === true ? "" : props[prop]);
+      newElement.setAttribute(prop, props[prop] === true ? "" : props[prop]);
     });
   }
-  if (html) element.innerHTML += html;
-  if (children) {
-    if (children.length) {
-      /* Iterators exist */ [...children].forEach((child) => {
-        typeof child === "string" ? element.appendChild(document.createTextNode(child)) : element.appendChild(child);
-      });
-    } else {
-      typeof children === "string" ? element.appendChild(document.createTextNode(children)) : element.appendChild(children);
-    }
+  if (children && Array.isArray(children)) {
+    children.forEach((child) => {
+      if (typeof child === "string") {
+        newElement.appendChild(document.createTextNode(child));
+      } else if (child instanceof HTMLElement) {
+        newElement.appendChild(child);
+      } else {
+        newElement.appendChild(create(child));
+      }
+    });
   }
-  return element;
-};
+  return newElement;
+}
+
 /**
- * Create element with args append to target
- * @param map Map of key:selector, value:args
+ * Replace elements
+ * @param matched: Element array to be replaced
+ * @param args: New element arguments will call create
  */
-export const retag = (map) => {
-  if (!map) return;
-  for (const [key, args] of map) {
-    for (let i = document.querySelectorAll(key).length - 1; i >= 0; i--) {
-      const element = document.querySelectorAll(key)[i];
-      const tag = (typeof args === "string" ? args : args?.tag) || "div";
-      const props = args.props || {};
-      const { children, html = "" } = args;
-      [...element.attributes].reduce((acc, attr) => {
-        acc[attr.name] = attr.args;
-        return acc;
-      }, props);
-      const newElement = create({
-        tag,
-        html: html + element.innerHTML,
-        props,
-        children,
-      });
-      element.parentNode.replaceChild(newElement, element);
+export function retag(matched: HTMLElement[], args: string | EleArgs | HTMLElement) {
+  for (let i = matched.length - 1; i >= 0; i--) {
+    const element = matched[i];
+    const newElement = create(args);
+    for (let i = 0; i < element.attributes.length; i++) {
+      const attribute = element.attributes[i];
+      if (!newElement.hasAttribute(attribute.name)) {
+        newElement.setAttribute(attribute.name, attribute.value);
+      }
     }
+    newElement.innerHTML = element.innerHTML;
+    element.parentNode?.replaceChild(newElement, element);
   }
+}
+
+export type EleArgs = {
+  tag: string;
+  props?: Record<string, any>;
+  children?: Array<string | HTMLElement | EleArgs>;
 };
