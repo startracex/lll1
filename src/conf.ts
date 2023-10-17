6318a056352c1f0061580e1dfdb04b73b01b6925
STARTRACEX <startraceebr@foxmail.com>
2023-10-17 14:39:32 +0800

    Move the export declaration

--- src/conf.ts
+++ src/conf.ts
@@ -9,17 +9,21 @@ const defconf: conftype = {
   reflect: false,
   cssvar: "godown-c",
 };
-const conf: conftype = init(globalThis.GodownWebComponentsCONF, defconf);
-function init(CONFObject: conftype, source = conf) {
+
+export const conf: conftype = init(globalThis.GodownWebComponentsCONF, defconf);
+export default conf;
+
+export function init(CONFObject: conftype, source = conf) {
   Object.assign(source, CONFObject);
   if (source.reflect) {
     // Reflect conf to globalThis
     globalThis.GodownWebComponentsCONF = source;
+  } else {
+    delete globalThis.GodownWebComponentsCONF;
   }
   return source;
 }
-export { conf, init };
-export default conf;
+
 declare global {
   interface globalThis {
     GodownWebComponentsCONF: conftype;
