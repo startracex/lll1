35078f0c5256d80a75e972fe66d613f12cf99cd2
STARTRACEX <startraceebr@foxmail.com>
2023-10-21 23:32:18 +0800

    Modify exports sort

--- src/deps.ts
+++ src/deps.ts
@@ -1,7 +1,7 @@
-export { CSSResultGroup, html, css, LitElement } from "lit";
+export { conf, init } from "./conf.js";
+export { GlobalSTD, cssvar, DisableWarning } from "./glob.js";
+export { append, create, define, retag, constructCSS } from "./with.js";
+export { CSSResultGroup, html, css, LitElement, unsafeCSS } from "lit";
 export { query, queryAll, property, state, customElement } from "lit/decorators.js";
-export { conf } from "./conf.js";
-export { append, create, define, retag } from "./with.js";
 export { ifDefined } from "lit/directives/if-defined.js";
 export { classMap } from "lit/directives/class-map.js";
-export { GLOBSTD, cssvar, DisableWarning } from "./glob.js";
