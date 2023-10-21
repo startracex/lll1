622efa66dd90cbbc7e120bc492b006dcb08fd298
STARTRACEX <startraceebr@foxmail.com>
2023-10-21 23:15:17 +0800

    Rename and extract

--- src/glob.ts
+++ src/glob.ts
@@ -1,15 +1,18 @@
 import { css, CSSResultGroup, LitElement, unsafeCSS, type WarningKind } from "lit";
 import { conf } from "./conf.js";
+
 /**
  * Custom CSS variable,join with "--" ,only allow `a-Z,0-9,-`
  */
 export const cssvar = unsafeCSS("--" + conf.cssvar.replace(/[^a-zA-Z0-9\\-]/g, ""));
-export class GLOBSTD extends LitElement {
+
+export class GlobalSTD extends LitElement {
   static styles = css`
     :host {
       ${cssvar}--text-selection: rgb(80 255 255);
       ${cssvar}--text-selection-background: rgb(0 0 0 / 10%);
     }
+
     * {
       font-size: 100%;
       color: inherit;
@@ -17,11 +20,28 @@ export class GLOBSTD extends LitElement {
       margin: 0;
       padding: 0;
     }
+
     ::selection {
       color: var(${cssvar}--text-selection);
       background: var(${cssvar}--text-selection-background);
     }
   ` as CSSResultGroup;
+
+  get assigned() {
+    return this._slot?.assignedElements() as HTMLElement[];
+  }
+
+  get _slot() {
+    return this.shadowRoot.querySelector("slot");
+  }
+
+  get offsetsWidth() {
+    return this.offsetParent?.clientWidth ?? document.body.offsetWidth;
+  }
+
+  get offsetsHeight() {
+    return this.offsetParent?.clientHeight ?? document.body.offsetHeight;
+  }
 }
 
 export function DisableWarning(e: { disableWarning?: (warningKind: WarningKind) => void }, ...w: WarningKind[]) {
