d21431ebfc7e1353377c87bf540fba6934f7f7fa
STARTRACEX <startraceebr@foxmail.com>
2023-10-24 21:03:07 +0800

    Update global's method, style

--- src/glob.ts
+++ src/glob.ts
@@ -9,16 +9,19 @@ export const cssvar = unsafeCSS("--" + conf.cssvar.replace(/[^a-zA-Z0-9\\-]/g, "
 export class GlobalSTD extends LitElement {
   static styles = css`
     :host {
-      ${cssvar}--text-selection: rgb(80 255 255);
-      ${cssvar}--text-selection-background: rgb(0 0 0 / 10%);
+      ${cssvar}--text-selection: inherit;
+      ${cssvar}--text-selection-background: none;
     }
 
     * {
       font-size: 100%;
+      font-style: normal;
       color: inherit;
       box-sizing: border-box;
       margin: 0;
       padding: 0;
+      border: 0;
+      outline: 0;
     }
 
     ::selection {
@@ -31,8 +34,12 @@ export class GlobalSTD extends LitElement {
     return this._slot?.assignedElements() as HTMLElement[];
   }
 
+  get assignedNodes() {
+    return this._slot?.assignedNodes();
+  }
+
   get _slot() {
-    return this.shadowRoot.querySelector("slot");
+    return this.shadowRoot.querySelector("slot:not([name])") as HTMLSlotElement;
   }
 
   get offsetsWidth() {
@@ -41,6 +48,76 @@ export class GlobalSTD extends LitElement {
 
   get offsetsHeight() {
     return this.offsetParent?.clientHeight ?? document.body.offsetHeight;
+  }
+
+  protected EventsCollection = new Map<string, Map<any, Set<ListenerFunc>>>();
+  protected ListenersAlias = new Map<string, ListenerFunc>();
+
+  addEvent(src: any, type: string, listener: ListenerFunc, alias?: string) {
+    const gotType = this.EventsCollection.get(type);
+    if (gotType) {
+      const gotEle = gotType.get(src);
+      if (gotEle) {
+        gotEle.add(listener);
+      } else {
+        const s = new Set<ListenerFunc>();
+        s.add(listener);
+        gotType.set(src, s);
+      }
+    } else {
+      const s = new Set<ListenerFunc>();
+      s.add(listener);
+      const m = new Map<any, Set<ListenerFunc>>();
+      m.set(src, s);
+      this.EventsCollection.set(type, m);
+    }
+    src.addEventListener(type, listener);
+    if (alias) {
+      this.ListenersAlias.set(alias, listener);
+    }
+  }
+
+  removeEvent(src: any, type: string, id: string | ListenerFunc) {
+    const gotType = this.EventsCollection.get(type);
+    if (gotType) {
+      const gotEle = gotType.get(src);
+      if (gotEle) {
+        if (typeof id === "string") {
+          const listener = this.ListenersAlias.get(id);
+          if (listener) {
+            gotEle.delete(listener);
+            src.removeEventListener(type, listener);
+          }
+        } else {
+          gotEle.forEach((listener) => {
+            src.removeEventListener(type, listener);
+          });
+        }
+        if (gotEle.size === 0) {
+          gotType.delete(src);
+        }
+        if (gotType.size === 0) {
+          this.EventsCollection.delete(type);
+        }
+      }
+    }
+  }
+
+  removeAllEvents() {
+    this.EventsCollection.forEach((typeMap, type) => {
+      typeMap.forEach((eventListeners, src) => {
+        eventListeners.forEach((listener) => {
+          src.removeEventListener(type, listener);
+        });
+      });
+      typeMap.clear();
+      this.EventsCollection.delete(type);
+    });
+    this.ListenersAlias.clear();
+  }
+
+  disconnectedCallback() {
+    this.removeAllEvents();
   }
 }
 
@@ -53,3 +130,5 @@ export function DisableWarning(e: { disableWarning?: (warningKind: WarningKind) 
     e.disableWarning?.("change-in-update");
   }
 }
+
+type ListenerFunc = (...args: any[]) => any;
