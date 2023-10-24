bb987c09b7e30a12a5ce6b9c221953b64c5ef844
STARTRACEX <startraceebr@foxmail.com>
2023-10-24 21:13:53 +0800

    Creat clip-text

---
+++ src/effect/clip-text.ts
@@ -0,0 +1,28 @@
+import { css, define, property } from "../deps.js";
+import EffectSTD from "./std.js";
+import { htmlSlot } from "../tmpl.js";
+
+@define("clip-text")
+export class ClipText extends EffectSTD {
+  @property() text = "";
+  static styles = css`
+    :host {
+      color: transparent;
+      background-color: currentColor;
+      -webkit-background-clip: text !important;
+      background-clip: text !important;
+    }
+  `;
+
+  render() {
+    return this.text || htmlSlot();
+  }
+}
+
+export default ClipText;
+
+declare global {
+  interface HTMLElementTagNameMap {
+    "clip-text": ClipText;
+  }
+}
