4b03d363175b2e8f95cacc4f04c6a73686f70a2e
STARTRACEX <startraceebr@foxmail.com>
2023-12-01 15:08:08 +0800

    Fix create's type errors

--- src/react/create.ts
+++ src/react/create.ts
@@ -1,12 +1,20 @@
+import { createComponent, EventName, Options, ReactWebComponent } from "@lit/react";
 import conf from "../conf.js";
-import { createComponent } from "@lit/react";
 import * as React from "react";
 
-export const create = (option: Omit<Parameters<typeof createComponent>[0], "react">) => {
+export const create = <
+  /**/
+  I extends HTMLElement,
+  E extends Record<string, EventName | string> = NonNullable<unknown>,
+>(
+  /**/
+  option: Omit<Options<I, E>, "react">,
+  ns: typeof React = React,
+): ReactWebComponent<I, E> => {
   option.tagName = conf.namemap.get(option.tagName) || option.tagName;
-  return createComponent({
+  return createComponent<I, E>({
     ...option,
-    react: React,
+    react: ns,
   });
 };
 
