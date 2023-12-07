a5a5dccddd8b0a84df4db0ae85c12e14b880c74d
STARTRACEX <startraceebr@foxmail.com>
2023-12-08 01:39:26 +0800

    Update methods

--- src/glob.ts
+++ src/glob.ts
@@ -1,4 +1,5 @@
 import { css, CSSResultGroup, LitElement, unsafeCSS, type WarningKind } from "lit";
+import { EventListenerFunc, EventsCollection } from "./lib/event-collection.js";
 import { conf } from "./conf.js";
 
 /**
@@ -38,8 +39,19 @@ export class GlobalSTD extends LitElement {
     return this._slot?.assignedNodes();
   }
 
+  /**
+   * no named slot
+   */
   get _slot() {
     return this.shadowRoot.querySelector("slot:not([name])") as HTMLSlotElement;
+  }
+
+  get slottedChildren() {
+    return [...this.querySelectorAll<HTMLElement>("[slot]")];
+  }
+
+  get slottedNames() {
+    return this.slottedChildren.map((c) => c.getAttribute("slot")).filter((v) => v);
   }
 
   get offsetsWidth() {
@@ -50,70 +62,18 @@ export class GlobalSTD extends LitElement {
     return this.offsetParent?.clientHeight ?? document.body.offsetHeight;
   }
 
-  protected EventsCollection = new Map<string, Map<any, Set<ListenerFunc>>>();
-  protected ListenersAlias = new Map<string, ListenerFunc>();
+  __events = new EventsCollection();
 
-  addEvent(src: any, type: string, listener: ListenerFunc, alias?: string) {
-    const gotType = this.EventsCollection.get(type);
-    if (gotType) {
-      const gotEle = gotType.get(src);
-      if (gotEle) {
-        gotEle.add(listener);
-      } else {
-        const s = new Set<ListenerFunc>();
-        s.add(listener);
-        gotType.set(src, s);
-      }
-    } else {
-      const s = new Set<ListenerFunc>();
-      s.add(listener);
-      const m = new Map<any, Set<ListenerFunc>>();
-      m.set(src, s);
-      this.EventsCollection.set(type, m);
-    }
-    src.addEventListener(type, listener);
-    if (alias) {
-      this.ListenersAlias.set(alias, listener);
-    }
+  addEvent(src: any, type: string, listener: EventListenerFunc, alias?: string) {
+    this.__events.addEvent(src, type, listener, alias);
   }
 
-  removeEvent(src: any, type: string, id: string | ListenerFunc) {
-    const gotType = this.EventsCollection.get(type);
-    if (gotType) {
-      const gotEle = gotType.get(src);
-      if (gotEle) {
-        if (typeof id === "string") {
-          const listener = this.ListenersAlias.get(id);
-          if (listener) {
-            gotEle.delete(listener);
-            src.removeEventListener(type, listener);
-          }
-        } else {
-          gotEle.forEach((listener) => {
-            src.removeEventListener(type, listener);
-          });
-        }
-        if (gotEle.size === 0) {
-          gotType.delete(src);
-        }
-        if (gotType.size === 0) {
-          this.EventsCollection.delete(type);
-        }
-      }
-    }
+  removeEvent(src: any, type: string, id: string | EventListenerFunc) {
+    this.__events.removeEvent(src, type, id);
   }
 
   removeAllEvents() {
-    this.EventsCollection.forEach((typeMap, type) => {
-      typeMap.forEach((eventListeners, src) => {
-        eventListeners.forEach((listener) => {
-          src.removeEventListener(type, listener);
-        });
-      });
-      typeMap.clear();
-      this.EventsCollection.delete(type);
-    });
-    this.ListenersAlias.clear();
+    this.__events.removeAllEvents();
   }
 
   disconnectedCallback() {
@@ -143,5 +103,3 @@ export function DisableWarning(e: { disableWarning?: (warningKind: WarningKind) 
     e.disableWarning?.("change-in-update");
   }
 }
-
-type ListenerFunc = (...args: any[]) => any;
