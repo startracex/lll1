20e3fdcc76623a2a44a716d48efe1740c1c52fc6
STARTRACEX <startraceebr@foxmail.com>
2024-01-14 03:21:50 +0800

    Fix deps

--- src/deps.ts
+++ src/deps.ts
@@ -1,6 +1,6 @@
 export { conf, init } from "./conf.js";
-export { GodownElement, cssvar } from "./glob.js";
-export { append, create, define, retag, constructCSS } from "./with.js";
+export { GodownElement, cssvar, cssvarValues, define, createScope } from "./glob.js";
+export { append, create, retag, constructCSS } from "./with.js";
 export { html, css, LitElement, unsafeCSS } from "lit";
 export { query, queryAll, property, state, customElement } from "lit/decorators.js";
 export { ifDefined } from "lit/directives/if-defined.js";
