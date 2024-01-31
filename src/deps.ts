09fa0ca09c86368d8c973341a4d962471c673cab
STARTRACEX <startraceebr@foxmail.com>
2024-02-01 06:57:54 +0800

    Export nothing

--- src/deps.ts
+++ src/deps.ts
@@ -1,4 +1,4 @@
-export { html, css, unsafeCSS, type CSSResultGroup, type PropertyValueMap } from "lit";
+export { html, css, unsafeCSS, type CSSResultGroup, nothing, type PropertyValueMap } from "lit";
 export * from "lit/decorators.js";
 export * from "lit/directives/if-defined.js";
 export * from "lit/directives/class-map.js";
